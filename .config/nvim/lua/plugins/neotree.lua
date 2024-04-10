580b3468f733290700d41bb80216c6b6bf4dff24
Misha Mykolyshyn <mykhailo.mykolyshyn@alphanovel.io>
2024-04-10 14:28:41 +0300

    updated grep neotree shortcut

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -1,7 +1,14 @@
 local do_setcd = function(state)
   local p = state.tree:get_node().path
   print(p) -- show in command line
-  vim.cmd(string.format('exec(":lcd %s")', p))
+  vim.fn.chdir(p)
+end
+
+local do_grep = function(state)
+  local prev_dir = vim.fn.getcwd()
+  local grep_dir = state.tree:get_node().path
+  require("telescope.builtin").live_grep({ cwd = grep_dir })
+  vim.fn.chdir(prev_dir)
 end
 
 return {
@@ -12,13 +19,13 @@ return {
         do_setcd(state)
       end,
       grep = function(state)
-        do_setcd(state)
-        require("telescope.builtin").live_grep()
+        do_grep(state)
       end,
     },
     window = {
       mappings = {
         ["/"] = "grep",
+        ["<c-/>"] = "fuzzy_finder",
         ["g"] = "setcd",
       },
     },
