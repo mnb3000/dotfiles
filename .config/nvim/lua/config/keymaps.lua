8246659d9323c15204b5e8657a16225fe70bd674
mnb3000 <misha.mykolyshyn@gmail.com>
2024-07-10 13:27:48 +0300

    update lazyvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,18 +3,10 @@
 -- Add any additional keymaps here
 --
 
-vim.keymap.set(
-  "n",
-  "<C-d>",
-  [[<Cmd>lua vim.cmd('normal! <C-d>'); MiniAnimate.execute_after('scroll', 'normal! zvzz')<CR>]]
-)
-vim.keymap.set(
-  "n",
-  "<C-u>",
-  [[<Cmd>lua vim.cmd('normal! <C-u>'); MiniAnimate.execute_after('scroll', 'normal! zvzz')<CR>]]
-)
+vim.keymap.set("n", "<C-d>", "<C-d>zvzz")
+vim.keymap.set("n", "<C-u>", "<C-u>zvzz")
 
-vim.keymap.set("n", "n", [[<Cmd>lua vim.cmd('normal! n'); MiniAnimate.execute_after('scroll', 'normal! zvzz')<CR>]])
-vim.keymap.set("n", "N", [[<Cmd>lua vim.cmd('normal! N'); MiniAnimate.execute_after('scroll', 'normal! zvzz')<CR>]])
+vim.keymap.set("n", "n", "nzvzz")
+vim.keymap.set("n", "N", "Nzvzz")
 vim.keymap.del("n", "<leader>-")
 vim.keymap.set("n", "<leader>\\", "<C-W>s", { desc = "Split Window Below", remap = true })
