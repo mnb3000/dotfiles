6c9c6edb1429f428958a3abff8ab936dca5fa12a
mnb3000 <misha.mykolyshyn@gmail.com>
2024-12-28 15:24:46 +0200

    restore mini-animate in nvim

--- .config/nvim/lua/plugins/tokyonight.lua
+++ .config/nvim/lua/plugins/tokyonight.lua
@@ -1,4 +1,21 @@
 return {
   "folke/tokyonight.nvim",
-  opts = { style = "storm" },
+  opts = {
+    style = "moon",
+    -- transparent = true,
+
+    -- Storm BG settings
+    -- on_colors = function(colors)
+    --   colors.bg = "#222436"
+    --   colors.bg_dark = "#1e2030"
+    --   colors.bg_dark1 = "#191B29"
+    --   colors.bg_float = "#1e2030"
+    --   colors.bg_highlight = "#2f334d"
+    --   colors.bg_popup = "#1e2030"
+    --   colors.bg_search = "#3e68d7"
+    --   colors.bg_sidebar = "#1e2030"
+    --   colors.bg_statusline = "#1e2030"
+    --   colors.bg_visual = "#2d3f76"
+    -- end,
+  },
 }
