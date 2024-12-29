e98094084065e7b231b2a06c1fbdf7617280e9b5
mnb3000 <misha.mykolyshyn@gmail.com>
2024-12-29 17:49:33 +0200

    update nvim

---
+++ nvim/lua/plugins/typewriter.lua
@@ -0,0 +1,8 @@
+return {
+  -- dir = "~/projects/personal/stay-centered.nvim",
+  -- "mnb3000/stay-centered.nvim",
+  -- lazy = false,
+  -- opts = {
+  --   include_filetypes = { "markdown" },
+  -- },
+}
