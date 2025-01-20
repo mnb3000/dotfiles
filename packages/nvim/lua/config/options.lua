fb43d9cbb6025c54ce0e2bfb7a4679d6f1f65a15
mnb3000 <misha.mykolyshyn@gmail.com>
2025-01-21 00:40:21 +0200

    feat(nvim): add applescript syntax

--- packages/nvim/lua/config/options.lua
+++ packages/nvim/lua/config/options.lua
@@ -4,3 +4,9 @@
 --
 -- -- Prepend mise shims to PATH
 vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims" .. vim.env.PATH
+
+vim.filetype.add({
+  extension = {
+    applescript = "applescript",
+  },
+})
