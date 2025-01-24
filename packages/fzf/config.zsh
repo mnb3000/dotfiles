443686a34f518fdbac92b74541abc9ae549b29e0
mnb3000 <misha.mykolyshyn@gmail.com>
2025-01-24 04:11:09 +0200

    feat(fzf): new coherent colors

--- packages/fzf/config.zsh
+++ packages/fzf/config.zsh
@@ -1,33 +1,33 @@
 # TokyoNight FZF
 
-FZF_DEFAULT_OPTS=" \
-  --ansi \
-  --border=rounded \
-  --preview-window=border-rounded \
-  --height 80% \
-  --padding=1 \
-  --margin=1 \
-  --layout=reverse \
-  --color=bg+:#2e3c64 \
-  --color=bg:#1f2335 \
-  --color=border:#29a4bd \
-  --color=fg:#c0caf5 \
-  --color=gutter:#1f2335 \
-  --color=header:#ff9e64 \
-  --color=hl+:#2ac3de \
-  --color=hl:#2ac3de \
-  --color=info:#545c7e \
-  --color=marker:#ff007c \
-  --color=pointer:#ff007c \
-  --color=prompt:#2ac3de \
-  --color=query:#c0caf5:regular \
-  --color=scrollbar:#29a4bd \
-  --color=separator:#ff9e64 \
-  --color=spinner:#ff007c \
-"
+export FZF_DEFAULT_OPTS="\
+--ansi \
+--border=rounded \
+--preview-window=border-rounded \
+--height 100% \
+--padding=1 \
+--margin=1 \
+--layout=reverse \
+--color=bg+:#1A1E2F \
+--color=bg:#1A1E2F \
+--color=border:#7AA2F7 \
+--color=fg:#C0CBF4 \
+--color=gutter:#1D202F \
+--color=header:#E0AF67 \
+--color=hl+:#4FD6BE \
+--color=hl:#4FD6BE \
+--color=info:#404869 \
+--color=marker:#F7768D \
+--color=pointer:#F7768D \
+--color=prompt:#4FD6BE \
+--color=query:#C0CBF4:regular \
+--color=scrollbar:#7AA2F7 \
+--color=separator:#E0AF67 \
+--color=spinner:#F7768D"
+
 if is-ish
 then
-  FZF_DEFAULT_OPTS+="--info=inline"
+  export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --info=inline"
 else
-  FZF_DEFAULT_OPTS+="--info=inline-right"
+  export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS --info=inline-right"
 fi
