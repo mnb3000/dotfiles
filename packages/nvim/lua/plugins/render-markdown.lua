40c1dd68164185b990e3e9c41bff795904ac2444
mnb3000 <misha.mykolyshyn@gmail.com>
2025-02-18 07:48:43 +0200

    feat(nvim): add header signs to markdown

---
+++ packages/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,15 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    code = {
+      sign = true,
+    },
+    heading = {
+      sign = true,
+      -- icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+    },
+    checkbox = {
+      enabled = true,
+    },
+  },
+}
