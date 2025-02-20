9edfe773627c1d99b0c9d6fb908c33960b19358e
mnb3000 <misha.mykolyshyn@gmail.com>
2025-02-21 01:49:14 +0200

    fix(nvim): update tine-glimmer config

--- packages/nvim/lua/plugins/tiny-glimmer.lua
+++ packages/nvim/lua/plugins/tiny-glimmer.lua
@@ -3,12 +3,13 @@ return {
   event = "VeryLazy",
   opts = {
     overwrite = {
+      -- auto_map = false,
       default_animation = {
         name = "fade",
 
         settings = {
-          max_duration = 500,
-          min_duration = 500,
+          max_duration = 750,
+          min_duration = 750,
 
           from_color = "DiffDelete",
           to_color = "Normal",
@@ -19,56 +20,15 @@ return {
         enabled = true,
         default_animation = "fade",
       },
-      search = {
-        enabled = true,
-        default_animation = "pulse",
-
-        -- Keys to navigate to the next match
-        next_mapping = "nzzzv",
-
-        -- Keys to navigate to the previous match
-        prev_mapping = "Nzzzv",
-      },
       paste = {
         enabled = true,
         default_animation = "reverse_fade",
-
-        -- Keys to paste
-        paste_mapping = "p",
-
-        -- Keys to paste above the cursor
-        Paste_mapping = "P",
       },
       undo = {
         enabled = true,
-
-        default_animation = {
-          name = "fade",
-
-          settings = {
-            from_color = "DiffDelete",
-
-            max_duration = 500,
-            min_duration = 500,
-          },
-        },
-        undo_mapping = "u",
       },
       redo = {
         enabled = true,
-
-        default_animation = {
-          name = "fade",
-
-          settings = {
-            from_color = "DiffAdd",
-
-            max_duration = 500,
-            min_duration = 500,
-          },
-        },
-
-        redo_mapping = "<c-r>",
       },
     },
   },
