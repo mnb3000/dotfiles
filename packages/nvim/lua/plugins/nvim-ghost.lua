9c429ff2e16db6beac399490b64ca488fd439eb7
mnb3000 <misha.mykolyshyn@gmail.com>
2025-03-27 21:34:12 +0200

    chore(nvim): remove nvim-ghost, bump plugins

--- packages/nvim/lua/plugins/nvim-ghost.lua
+++ packages/nvim/lua/plugins/nvim-ghost.lua
@@ -1,8 +1,8 @@
-if vim.fn.has("mac") == 1 then
-  return {
-    "subnut/nvim-ghost.nvim",
-    event = "VeryLazy",
-  }
-else
-  return {}
-end
+-- if vim.fn.has("mac") == 1 then
+--   return {
+--     "subnut/nvim-ghost.nvim",
+--     event = "VeryLazy",
+--   }
+-- else
+return {}
+-- end
